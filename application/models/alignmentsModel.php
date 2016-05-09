5e42584043a196e112ac562eef304634f5d654ad
Job Cuppen <jobcuppen@live.nl>
2016-05-09 17:25:58 +0200

    initial commit

---
+++ application/models/alignmentsModel.php
@@ -0,0 +1,7 @@
+<?php
+  class AlignmetsModel extends CI_Model {
+    function __construct() {
+      $this->load->database();
+    }
+  }
+?>
---
+++ application/views/templates/header.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>
+      <?php
+        echo $title; 
+      ?>
+    </title>
+  </head>
+  <body>
+    <h1>
+      <?php 
+        echo $title;
+      ?>
+    </h1>
